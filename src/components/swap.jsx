97e027c367e1ea57416b4e21b32681a68e4d0ab1
conlot-crypto <denis.vasilev@yahoo.com>
2022-03-28 12:55:59 -0500

    enable hex tokenId

--- src/components/swap.jsx
+++ src/components/swap.jsx
@@ -1,14 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState } from 'react';
+import BigNumber from 'bignumber.js';
 
 import { connectWithMetamask, swapNFT } from '../data/contract';
 import { oldTokenIds } from '../data/oldNFTs';
 
 const SwapComp = ({ onNofity }) => {
-  const [tokenID, setTokenId] = useState(0);
+  const [tokenIDStr, setTokenIDStr] = useState(0);
   const [isSwapping, setSwapping] = useState(false);
 
   const handleSwap = async () => {
+    let tokenID = tokenIDStr;
+    if (tokenID.toString().startsWith('0x')) {
+      const dd = new BigNumber(tokenIDStr, 16);
+      tokenID = dd.toString(10);
+    }
+
     if (!oldTokenIds.includes(tokenID)) {
       onNofity('Invalid tokenID', false);
       return;
@@ -35,12 +42,12 @@ const SwapComp = ({ onNofity }) => {
         style={{ color: 'black', width: '100%', backgroundColor: 'lightgrey' }}
         type='text'
         placeholder='Please type tokenId of your old NFT'
-        value={tokenID}
-        onChange={(e) => setTokenId(e.target.value)}
+        value={tokenIDStr}
+        onChange={(e) => setTokenIDStr(e.target.value)}
       />
       <div
         onClick={handleSwap}
-        disabled={isSwapping || tokenID === ''}
+        disabled={isSwapping || tokenIDStr === ''}
         className={`btn btn-mint btn-lg page-scroll ${
           isSwapping ? '' : 'btn-disabled'
         }`}
